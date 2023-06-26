71eb53919e25a17af6fe99d62b488000a614ca8a
Alfonzo Lunar <102769417+alfonzolunar@users.noreply.github.com>
2023-06-26 16:37:49 -0500

    Initial commit

---
+++ JS/test.js
@@ -0,0 +1,13 @@
+function TestIF() {
+
+    const mayorEdad = 22;
+
+    if (mayorEdad < 18) {
+        console.log("Es menor  de edad");
+    } else if (mayorEdad > 18 && mayorEdad <25) {
+        console.log("Es un adulto joven"); 
+    } else {
+        console.log("Es mayor de edad");
+    }
+    }
+    
